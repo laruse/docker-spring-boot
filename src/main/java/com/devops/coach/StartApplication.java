e562d7d7e74b77a1ba1cf1878687048bd489d6e4
laruse <naoual.elhila@gmail.com>
2023-11-01 09:50:44 +0100

    Update StartApplication.java

--- src/main/java/com/devops/coach/StartApplication.java
+++ src/main/java/com/devops/coach/StartApplication.java
@@ -12,8 +12,8 @@ public class StartApplication {
 
     @GetMapping("/")
     public String index(final Model model) {
-        model.addAttribute("title", "Hello Abdelilah wach rak ca va?!");
-        model.addAttribute("msg", "Hello All..We are deploying springboot application into EKS cluster using Helm + Jenkins Pipeline!!!! Today is Aug 7th, 2023");
+        model.addAttribute("title", "Shadowing CloudBox");
+        model.addAttribute("msg", "Hello All..We are deploying springboot application into EKS cluster using Helm + Jenkins Pipeline!");
         return "index";
     }
 
